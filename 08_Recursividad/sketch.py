ba56481f73ec2a8f255bd6e47666aab6d2bb0587
Xavier Vergara <xaviervergara00@gmail.com>
2025-11-21 20:49:27 -0300

    ejercicio_3

---
+++ 08_Recursividad/sketch.py
@@ -0,0 +1,37 @@
+def potencia(n, m):
+    if m == 0:
+        return 1
+    else:
+        return n * potencia(n, m -1)
+
+
+
+
+# def potencia(2, 3):
+#     if m == 0:
+#         return 1
+#     else:
+#         return 2 * potencia(2, 2)
+
+
+# def potencia(2, 2):
+#     if m == 0:
+#         return 1
+#     else:
+#         return 2 * potencia(2, 1)
+
+
+# def potencia(2, 1):
+#     if m == 0:
+#         return 1
+#     else:
+#         return 2 * potencia(2, 0)
+
+
+# def potencia(2, 0):
+#     if m == 0:
+#         return 1
+
+
+
+ 
